92579822d19de752b2c86160f316fcbba875f0a0
Hartawan Bahari M <46642970+darkun7@users.noreply.github.com>
2021-03-24 05:32:49 +0700

    Update pretty_print.py

--- pretty_print.py
+++ pretty_print.py
@@ -1,9 +1,11 @@
-def sep(text = ''):
+def sep(text='', n=40):
   if text == '':
-    text = "-"
+    text = "="
   else:
     text = "["+text+"]"
-  print("=-=-=-=-=-="+text+"=-=-=-=-=-=")
+  amount = '{:=^'+str(n)+'}'
+  text = amount.format(text)
+  print(text)
 def br(line=1):
   for i in range(line):
     print('')
